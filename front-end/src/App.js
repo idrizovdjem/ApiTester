7d8968d05499e1eb9df27e7f6c18be90a5c05dbe
idrizovdjem <idrizovdjem@gmail.com>
2021-05-10 20:27:32 +0300

    Add Navigation component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,11 @@
-function App() {
-  return (
-    <div className="App">
-      Works!
-    </div>
-  );
+import Navigation from './components/Navigation/Navigation';
+
+const App = () => {
+	return (
+		<div className="App">
+			<Navigation />
+		</div>
+	);
 }
 
 export default App;
---
+++ front-end/src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import classes from './Navigation.module.css';
+
+const Navigation = () => {
+    return (
+        <nav className={classes.Navigation}>
+            <h3 className={classes.Logo}>Api Tester</h3>
+        </nav>
+    );
+}
+
+export default Navigation;
