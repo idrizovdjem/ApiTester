01d3ef57afc1121d3293dbab95801df951413ef4
idrizovdjem <idrizovdjem@gmail.com>
2021-05-11 19:06:14 +0300

    Add code editor to Body component

--- front-end/src/components/Body/Body.js
+++ front-end/src/components/Body/Body.js
@@ -1,9 +1,39 @@
 import classes from './Body.module.css';
+import AceEditor from "react-ace";
+
+import "ace-builds/src-noconflict/mode-json";
+import "ace-builds/src-noconflict/mode-xml";
+import "ace-builds/src-noconflict/theme-github";
+
 
 const Body = () => {
     return (
         <div className={classes.Body}>
+            <div className={classes.BodyOptions}>
+                <h3 className={classes.BodyTypeLabel}>Body Type: </h3>
+                <select className={classes.BodyTypeSelect}>
+                    <option>JSON</option>
+                    <option>XML</option>
+                    <option>Form URL Encoded</option>
+                    <option>No Body</option>
+                </select>
+            </div>
 
+            <div className={classes.Editor}>
+                <AceEditor
+                    mode="json"
+                    theme="github"
+                    name="UNIQUE_ID_OF_DIV"
+                    fontSize='3vh'
+                    editorProps={{ $blockScrolling: true }}
+                    style={{
+                        marginTop: '1vh',
+                        width: '74%',
+                        float: 'left',
+                        height: '75vh'
+                    }}
+                />
+            </div>
         </div>
     );
 }
