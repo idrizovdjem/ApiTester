255daf7cff121da766e18dde8fc419d18dce771c
idrizovdjem <idrizovdjem@gmail.com>
2021-05-11 17:47:50 +0300

    Implement header saving

--- front-end/src/components/Headers/HeaderControll/HeaderControll.js
+++ front-end/src/components/Headers/HeaderControll/HeaderControll.js
@@ -2,44 +2,57 @@ import { useState } from 'react';
 import classes from './HeaderControll.module.css';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faTrash, faSave } from '@fortawesome/free-solid-svg-icons';
 
 const HeaderControll = (props) => {
     const [headerKey, setHeaderKey] = useState(props.headerKey);
     const [headerValue, setHeaderValue] = useState(props.headerValue);
+    const [isSaved, setIsSaved] = useState(true);
 
     const changeHeaderKeyHandler = (event) => {
         const newHeaderKey = event.target.value;
         setHeaderKey(newHeaderKey);
-        props.updateHeader(props.index, { key: newHeaderKey, value: headerValue });
+        setIsSaved(false);
     }
 
     const changeHeaderValueHandler = (event) => {
         const newHeaderValue = event.target.value;
         setHeaderValue(newHeaderValue);
-        props.updateHeader(props.index, { key: headerKey, value: newHeaderValue });
+        setIsSaved(false);
+    }
+
+    const saveHeaderChangesHandler = () => {
+        props.updateHeader(props.index, { key: headerKey, value: headerValue });
+        setIsSaved(true);
     }
 
     return (
         <div className={classes.HeaderControll}>
-            <input 
-                className={classes.HeaderKeyInput} 
-                placeholder='Header Key' 
+            <input
+                className={classes.HeaderKeyInput}
+                placeholder='Header Key'
                 value={headerKey}
                 onChange={changeHeaderKeyHandler}
             />
 
-            <input 
-                className={classes.HeaderValueInput} 
+            <input
+                className={classes.HeaderValueInput}
                 placeholder='Header Value'
                 value={headerValue}
                 onChange={changeHeaderValueHandler}
             />
 
             <FontAwesomeIcon
+                style={{ color: isSaved ? 'darkslategrey' : 'aliceblue' }}
+                onClick={saveHeaderChangesHandler}
+                icon={faSave}
+                className={classes.HeaderIcon}
+            />
+
+            <FontAwesomeIcon
                 onClick={() => props.deleteHeader(props.index)}
                 icon={faTrash}
-                className={classes.HeaderDeleteIcon}
+                className={classes.HeaderIcon}
             />
         </div>
     );
