8a2833a6a5d29c55be4aeef0dd5f335be4eff590
idrizovdjem <idrizovdjem@gmail.com>
2021-05-12 15:10:03 +0300

    Implement dynamic form controlls add

--- front-end/src/components/Body/FormUrlEncodedEditor/FormUrlEncodedEditor.js
+++ front-end/src/components/Body/FormUrlEncodedEditor/FormUrlEncodedEditor.js
@@ -1,11 +1,26 @@
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import classes from './FormUrlEncodedEditor.module.css';
 
 import FormControll from './FormControll/FormControll';
 
 const FormUrlEncodedEditor = () => {
+    const [formControlls, setFormControlls] = useState([]);
+
+    const addFormControllHandler = () => {
+        setFormControlls(oldFormControlls => {
+            return [...oldFormControlls, { key: '', value:'' }];
+        });
+    }
+
     return (
         <div className={classes.FormUrlEncodedEditor}>
-            <FormControll />
+            {
+                formControlls.map(formControll => {
+                    return <FormControll key={uuidv4()} />
+                })
+            }
+            <button onClick={addFormControllHandler} className={classes.AddFormControllButton}>Add Form Controll</button>
         </div>
     );
 }
