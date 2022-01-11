3a1b5f070abf6272c97afc68d326c61a1c148669
Dzhem <idrizovdjem@gmail.com>
2022-01-11 22:14:20 +0200

    Add main folder

---
+++ front-end/src/components/main/Main.tsx
@@ -0,0 +1,13 @@
+import SearchSection from './searchSection/SearchSection';
+import TabsPanel from './tabsPanel/TabsPanel';
+
+const Main = () => {
+    return (
+        <main>
+            <SearchSection />
+            <TabsPanel />
+        </main>
+    );
+}
+
+export default Main;
